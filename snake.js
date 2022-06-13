8b6bbebc1f330265b73c29c0fcc21d64f6725439
Pedro Soares <pedro.soares@unico.io>
2022-06-12 21:04:02 -0400

    feat(Snake): add Snake class without move
    Added the Snake class with some possible moves, the board now draws the
    Snake initial state (with 1 block of size). This implementation does not
    contain the moves yet.

--- snake.js
+++ snake.js
@@ -25,13 +25,84 @@ class Board {
         this.ctx.clearRect(0, 0, this.ref.width, this.ref.height);
     }
 
-    drawRect (x, y, color) {
+    drawRect(x, y, color, width = this.ref.width, height = this.ref.height) {
         this.ctx.fillStyle = color;
-        this.ctx.fillRect(x, y, this.ref.width, this.ref.height);
+        this.ctx.fillRect(x, y, width, height);
+    }
+
+    drawSnake(snake) {
+        for (let i = 0; i < snake.body.length; i++) {
+            this.drawRect(
+                snake.body[i].x + 2.5,
+                snake.body[i].y + 2.5,
+                'white',
+                snake.size - 5,
+                snake.size - 5,
+            )
+        }
+    }
+}
+
+class Snake {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.body = [
+            {
+                x: this.x,
+                y: this.y,
+            }
+        ];
+        this.curMove = 'STOP';
+    }
+
+    get head() {
+        return this.body[this.body.length - 1];
+    }
+
+    get moves() {
+        return {
+            RIGHT: {
+                x: this.head.x + this.size,
+                y: this.head.y,
+            },
+            LEFT: {
+                x: this.head.x - this.size,
+                y: this.head.y,
+            },
+            UP: {
+                x: this.head.x,
+                y: this.head.y - this.size,
+            },
+            DOWN: {
+                x: this.head.x,
+                y: this.head.y + this.size,
+            },
+        }
+    }
+
+    move() {
+        if (!this.moves[this.curMove]) {
+            return;
+        }
+
+        this.body.push(this.moves[this.curMove]);
+        this.body.shift();
     }
 }
 
 const board = new Board();
+const snake = new Snake(
+    board.ref.width / 2,
+    board.ref.height / 2,
+    20
+);
+
+const updateState = () => {
+    board.clear();
+    snake.move();
+}
 
 const draw = () => {
     board.drawRect(0, 0, 'black');
@@ -40,11 +111,13 @@ const draw = () => {
     // TODO: add score based on snake size
     board.setScore();
 
+    board.drawSnake(snake)
+
     // TODO: draw apple
 }
 
 const show = () => {
-    board.clear();
+    updateState();
     draw();
 }
 
